5c4d9afddc5b86956575110c98c5683255d2151a
Mario Agustin Espinosa Roddriguez <amaka_2@hotmail.com>
2019-11-04 11:36:26 -0600

    Create meta.php

---
+++ template/meta.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+   <script src="../assets/js/meta.js">
+      // Configuracion Viewport
+      viewport : {
+         initScale = 1,
+         minScale = 1,
+         maxScale = 1,
+         width = 'device-width',
+         scalable = 'no'
+      }
+   </script>
+</html>
